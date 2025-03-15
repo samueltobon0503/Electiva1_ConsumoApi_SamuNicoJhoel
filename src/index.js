79624f8497164a16ac87adcd6dc66aeabca740aa
NicoRM1008 <restreponiko@hotmail.com>
2025-03-14 19:12:42 -0500

    Boostratp implemented

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import { HomeComponent } from './components/Home.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
